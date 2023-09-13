9af0da7c5bf127b1d58beb75c970ff215dd48bdd
Izaz Ahmed <izaz0303@gmail.com>
2023-09-13 21:02:23 +0530

    JPA-UUID

--- src/main/java/dev/izaz/productservice/models/Product.java
+++ src/main/java/dev/izaz/productservice/models/Product.java
@@ -1,14 +1,22 @@
 package dev.izaz.productservice.models;
 
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.Entity;
 
-@Setter
-@Getter
+@Entity
+@Builder
 public class Product extends BaseModel {
     private String title;
     private String description;
     private String image;
+    //            P : C
+    // => L to R: 1 : 1
+    // => R to L: m : 1
+    // => Ans:    m : 1
+    @ManyToOne
     private Category categery;
     private double price;
 

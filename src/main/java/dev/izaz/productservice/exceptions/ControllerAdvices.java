12af23d362457b61d933033b1270f0c8d54b2389
Izaz Ahmed <izaz0303@gmail.com>
2023-09-18 19:17:12 +0530

    comment added

--- src/main/java/dev/izaz/productservice/exceptions/ControllerAdvices.java
+++ src/main/java/dev/izaz/productservice/exceptions/ControllerAdvices.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerAdvices {
+    //@ResponseStatus(value = HttpStatus.NOT_FOUND) if we use this annotation then we don't need to write below method
+    //above annotation we have to use in created user defined exception class
     /*@ExceptionHandler(NotFoundException.class)
     private ResponseEntity<ExceptionDto> handleNotFoundException(NotFoundException notFoundException){
         //System.out.println("handle not found exception");
